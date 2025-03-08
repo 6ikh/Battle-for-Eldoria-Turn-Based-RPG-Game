8373df818484fb581e641929fc70bcb5d594b4e8
Parmindar Singh <parmindarmundra@gmail.com>
2025-03-08 18:04:34 -0500

    Adding DefendAction class implementing Action interface from goblin attack

---
+++ DefendAction.java
@@ -0,0 +1,6 @@
+public class DefendAction implements Action {
+    @Override
+    public void execute(Player player, Enemy enemy) {
+        System.out.println("You raise your shield, bracing for impact.");
+    }
+}
